806d6d229fa9117716e31cf531fede1d8bd0b302
Marco Navia <naviamarco@gmail.com>
2021-02-12 14:28:40 -0400

    Model update for 2020-2025 scenario

---
+++ Scripts/read_results_2025.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Minimalist example file showing how to read the results of a Dispa-SET run
+
+@author: Sylvain Quoilin
+"""
+
+# Add the root folder of Dispa-SET to the path so that the library can be loaded:
+import sys,os
+sys.path.append(os.path.abspath('../../Dispa-SET'))
+
+# Import Dispa-SET
+import dispaset as ds
+
+# Load the inputs and the results of the simulation
+inputs,results = ds.get_sim_results(path='../Simulations/bolivia_base_2025',cache=False)
+
+# if needed, define the plotting range for the dispatch plot:
+import pandas as pd
+rng = pd.date_range(start='2025-01-01',end='2025-12-31',freq='h')
+
+# Generate country-specific plots
+ds.plot_zone(inputs,results,rng=rng)
+
+# Bar plot with the installed capacities in all countries:
+cap = ds.plot_zone_capacities(inputs)
+
+# Bar plot with the energy balances in all countries:
+ds.plot_energy_zone_fuel(inputs,results,ds.get_indicators_powerplant(inputs,results))
+
+# Analyse the results for each country and provide quantitative indicators:
+r = ds.get_result_analysis(inputs,results)
+
+# Plot the reservoir levels
+ds.storage_levels(inputs,results)
